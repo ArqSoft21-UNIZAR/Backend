a18c7ec21ebe1a26952d3bd25367b5a0981763df
saladito <cesarborjamoreno@gmail.com>
2022-04-16 20:45:07 +0200

    string conexión bd

--- server/Model/dbManager.cs
+++ server/Model/dbManager.cs
@@ -5,19 +5,16 @@ using System.Drawing;
 
 public class dbManager
 {   
-    static private string user_bd = Environment.GetEnvironmentVariable("DATABASE_USER");
-    static private string password_bd = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
-    static private string host_bd = Environment.GetEnvironmentVariable("DATABASE_HOST");
-
-    static private string port_bd = Environment.GetEnvironmentVariable("DATABASE_PORT");
-
-    static private string id_bd = Environment.GetEnvironmentVariable("DATABASE_ID");
-    static private string postgresConfig = "User ID=" + user_bd + ";Password=" + password_bd + 
-                                           ";Host=" + host_bd + ";Port=" + port_bd + ";Database=" + id_bd + ";Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True";
+    static private string postgresConfig = "User ID=" + Environment.GetEnvironmentVariable("DATABASE_USER") +
+                                           ";Password=" + Environment.GetEnvironmentVariable("DATABASE_PASSWORD") + 
+                                           ";Host=" + Environment.GetEnvironmentVariable("DATABASE_HOST") + 
+                                           ";Port=" + Environment.GetEnvironmentVariable("DATABASE_PORT") + 
+                                           ";Database=" + Environment.GetEnvironmentVariable("DATABASE_ID") + 
+                                           ";Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True";
 
     public static NpgsqlConnection getDBConnection()
     {
-        NpgsqlConnection conn = new NpgsqlConnection("User ID="+Environment.GetEnvironmentVariable("DATABASE_USER")+";Password="+Environment.GetEnvironmentVariable("DATABASE_PASSWORD")+";Host="+Environment.GetEnvironmentVariable("DATABASE_HOST")+";Port="+Environment.GetEnvironmentVariable("DATABASE_PORT")+";Database="+Environment.GetEnvironmentVariable("DATABASE_ID")+";Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True;");
+        NpgsqlConnection conn = new NpgsqlConnection(postgresConfig);
         conn.Open();
         return conn;
     }
