1e45204e7a49518cfe800ec26820327d97ff7bbf
saladito <cesarborjamoreno@gmail.com>
2022-04-17 11:49:58 +0200

    script actualizado

--- server/publish.sh
+++ server/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
-docker build ./. --pull -f ./Dockerfile -t registry.heroku.com/meetme-b/web
+docker build -t meetme
+docker tag meetme registry.heroku.com/meetme-b/web
 docker push registry.heroku.com/meetme-b/web
 heroku container:release web -a meetme-b
