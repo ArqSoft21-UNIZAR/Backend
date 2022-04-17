faf072a4a9ab40c62ae0dc10da79606455871163
saladito <cesarborjamoreno@gmail.com>
2022-04-17 10:57:14 +0200

    prueba

--- server/Model/dbManager.cs
+++ server/Model/dbManager.cs
@@ -10,7 +10,7 @@ public class dbManager
                                            ";Host=" + Environment.GetEnvironmentVariable("DATABASE_HOST") + 
                                            ";Port=" + Environment.GetEnvironmentVariable("DATABASE_PORT") + 
                                            ";Database=" + Environment.GetEnvironmentVariable("DATABASE_ID") + 
-                                           ";Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True";
+                                           ";Pooling=true;TrustServerCertificate=True";
 
     public static NpgsqlConnection getDBConnection()
     {
