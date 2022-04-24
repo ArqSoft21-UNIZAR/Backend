542ba3bfc493d76b03562a382f013ea40eeac3d9
Marcos <marcosgarralaga@gmail.com>
2022-04-24 12:53:32 +0200

    Preparacion despliegue

--- server/Program.cs
+++ server/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(opt =>
         {
             opt.AddDefaultPolicy(builder =>
             {
-                builder.WithOrigins("http://localhost:4200")
+                builder.WithOrigins("https://meetme-arqsoft.herokuapp.com")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();;
