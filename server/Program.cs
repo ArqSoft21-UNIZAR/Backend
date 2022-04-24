343ccf4bd1f1d47063862f2e0c4c5f48b462ac7b
Marcos <marcosgarralaga@gmail.com>
2022-04-24 12:19:47 +0200

    ConexionHub semifuncional

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,5 @@
+using sever.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +8,10 @@ builder.Services.AddCors(opt =>
         {
             opt.AddDefaultPolicy(builder =>
             {
-                builder.AllowAnyOrigin()
+                builder.WithOrigins("http://localhost:4200")
                     .AllowAnyHeader()
-                    .AllowAnyMethod();
+                    .AllowAnyMethod()
+                    .AllowCredentials();;
             });
         });
 
@@ -17,6 +20,9 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 app.UseCors();
@@ -28,9 +34,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<MessageHub>("/MessageHub");
+});
+
+
+app.UseHttpsRedirection();
+
 
 app.MapControllers();
 
