d7bd8e11246fe4ef62045bbf195d0fbf6210e5e1
Marcos <marcosgarralaga@gmail.com>
2022-04-24 13:45:57 +0200

    Limpieza

--- server/Api/MessageHub.cs
+++ server/Api/MessageHub.cs
@@ -2,14 +2,14 @@ using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;  
   
 namespace sever.Controllers
-{  
-    public class MessageHub : Hub  
-    {  
+{
+    public class MessageHub : Hub
+    {
         public async Task NewMessage(Message msg)
         {
             Console.Write("Mensaje:", msg.message, " sender: ",msg.sender, " reciever: ", msg.reciever);
             await Clients.All.SendAsync(msg.reciever, msg);
             //TODO: Guardar el mensaje en chatsController.
-        }  
+        }
     }
 }
--- server/Model/VO/Message.cs
+++ server/Model/VO/Message.cs
@@ -1,8 +1,8 @@
-public class Message  
-{  
-    public string sender { get; set; }  
-    public string reciever { get; set; }  
-    public bool isSent { get; set; }  
-    public string message { get; set; }  
-    public DateTime date { get; set; }  
-}  
+public class Message
+{
+    public string sender { get; set; }
+    public string reciever { get; set; }
+    public bool isSent { get; set; }
+    public string message { get; set; }
+    public DateTime date { get; set; }
+}
