57080e3b8095a3c5bece1ffccce97e434f7b1ff0
Marcos <marcosgarralaga@gmail.com>
2022-05-03 22:23:25 +0200

    Mensajes de log

--- server/Api/matchesController.cs
+++ server/Api/matchesController.cs
@@ -15,6 +15,7 @@ public class matchesController : ControllerBase
     [HttpPost("request")]
     public async Task<IActionResult> requestMatch([FromBody] UserVO user)
     {
+        Console.Write("Ejecutado requestMatch con user = "+user.ToString()+"\n");
         if (await matchesLogic.requestNewMatch(user.email)){
             return Ok();
         }
@@ -27,12 +28,14 @@ public class matchesController : ControllerBase
     [HttpPost("get")]
     public async Task<IEnumerable<UserVO>> getMatches([FromBody] UserVO user)
     {
+        Console.Write("Ejecutado getMatch con user = "+user.ToString()+"\n");
         return await matchesLogic.getMatches(user.email);
     }
 
     [HttpPost("delete")]
     public async Task<IActionResult> deleteMatch([FromBody] MatchVO match)
     {
+        Console.Write("Ejecutado deleteMatch con user = "+match.ToString()+"\n");
         if (await matchesLogic.deleteMatch(match.email1, match.email2)){
             return Ok();
         }
